14d83668ad659737078503c1b5a15bd05dba5f26
Rami <rami.aouinti@gmail.com>
2026-04-19 07:50:38 +0200

    Handle legacy locale enum values in DBAL type

--- src/General/Domain/Doctrine/DBAL/Types/EnumLanguageType.php
+++ src/General/Domain/Doctrine/DBAL/Types/EnumLanguageType.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\General\Domain\Doctrine\DBAL\Types;
 
 use App\General\Domain\Enum\Language;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Override;
 
 /**
  * @package App\General
@@ -13,4 +15,19 @@ class EnumLanguageType extends EnumType
 {
     protected static string $name = Types::ENUM_LANGUAGE;
     protected static string $enum = Language::class;
+
+    /**
+     * Be tolerant to legacy/dirty values in production data.
+     */
+    #[Override]
+    public function convertToPHPValue($value, AbstractPlatform $platform): Language
+    {
+        $normalized = strtolower(trim((string)$value));
+
+        if ($normalized === 'uk') {
+            $normalized = Language::UA->value;
+        }
+
+        return Language::tryFrom($normalized) ?? Language::getDefault();
+    }
 }
--- src/General/Domain/Doctrine/DBAL/Types/EnumLocaleType.php
+++ src/General/Domain/Doctrine/DBAL/Types/EnumLocaleType.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\General\Domain\Doctrine\DBAL\Types;
 
 use App\General\Domain\Enum\Locale;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Override;
 
 /**
  * @package App\General
@@ -13,4 +15,19 @@ class EnumLocaleType extends EnumType
 {
     protected static string $name = Types::ENUM_LOCALE;
     protected static string $enum = Locale::class;
+
+    /**
+     * Be tolerant to legacy/dirty values in production data.
+     */
+    #[Override]
+    public function convertToPHPValue($value, AbstractPlatform $platform): Locale
+    {
+        $normalized = strtolower(trim((string)$value));
+
+        if ($normalized === 'uk') {
+            $normalized = Locale::UA->value;
+        }
+
+        return Locale::tryFrom($normalized) ?? Locale::getDefault();
+    }
 }
