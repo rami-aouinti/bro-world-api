dba0a78e6496a4052536273b0fa9eeae58024b9b
Rami <rami.aouinti@gmail.com>
2026-04-22 02:12:35 +0200

    Revert "feat(mercure): allow public publishes without JWT"

--- src/General/Application/Service/MercurePublisher.php
+++ src/General/Application/Service/MercurePublisher.php
@@ -19,6 +19,8 @@ final readonly class MercurePublisher
         private LoggerInterface $logger,
         #[Autowire('%env(string:MERCURE_PUBLISH_URL)%')]
         private string $mercurePublishUrl,
+        #[Autowire('%env(string:MERCURE_JWT_SECRET)%')]
+        private string $mercureJwtSecret,
     ) {
     }
 
@@ -41,6 +43,7 @@ final readonly class MercurePublisher
 
         try {
             $this->httpClient->request('POST', $this->mercurePublishUrl, [
+                'auth_bearer' => $this->mercureJwtSecret,
                 'body' => $payload,
             ])->getStatusCode();
         } catch (ExceptionInterface $exception) {
