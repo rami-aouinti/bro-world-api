8cb7182bc7afc6855488bfb3396e35283c786eb6
Rami <rami.aouinti@gmail.com>
2026-04-27 02:50:38 +0200

    feat(crm): add endpoints for my assigned tasks and task requests

---
+++ src/Crm/Transport/Controller/Api/V1/Task/ListMyAssignedTasksController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Crm\Transport\Controller\Api\V1\Task;
+
+use App\Crm\Application\Service\TaskBoardService;
+use App\Role\Domain\Enum\Role;
+use App\User\Domain\Entity\User;
+use OpenApi\Attributes as OA;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[AsController]
+#[OA\Tag(name: 'Crm')]
+#[IsGranted(Role::CRM_VIEWER->value)]
+final readonly class ListMyAssignedTasksController
+{
+    public function __construct(
+        private TaskBoardService $taskBoardService,
+    ) {
+    }
+
+    #[Route('/v1/crm/me/assigned-tasks', methods: [Request::METHOD_GET])]
+    #[OA\Get(
+        summary: 'List My Assigned Tasks',
+        responses: [
+            new OA\Response(response: JsonResponse::HTTP_OK, description: 'List of tasks assigned to the current user.'),
+        ],
+    )]
+    public function __invoke(User $loggedInUser): JsonResponse
+    {
+        $mine = $this->taskBoardService->listMine('crm-general-core', $loggedInUser);
+
+        return new JsonResponse([
+            'items' => $mine['items']['tasks'] ?? [],
+        ]);
+    }
+}
